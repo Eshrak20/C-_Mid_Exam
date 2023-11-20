b6d1bc0e5594b3403722c538991992eca31ecb0f
Eshrak <uemran2018@gmail.com>
2023-11-21 01:06:57 +0600

    sending more problem

--- Monkey.cpp
+++ Monkey.cpp
@@ -1,58 +1,31 @@
-// #include <stdio.h>
-// #include <iostream>
-// #include <bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-
-//     char x[100];
-//     char y[100];
-
-//     cin >> x;
-//     getchar();
-//     cin.getline(y, 100);
-
-//     for (size_t i = 0; i < strlen(x); i++)
-//     {
-//         x[i];
-//     }
-//     sort(x, x + strlen(x));
-//     cout << x;
-//     cout << endl;
-//     for (size_t i = 0; i < strlen(y); i++)
-//     {
-//         y[i];
-//     }
-//     sort(y, y + strlen(y));
-//     for (size_t i = 0; i < strlen(y); i++)
-//     {
-//         if (y[i] != ' ')
-//         {
-//             cout << y[i];
-//         }
-//     }
-
-//     return 0;
-// }
-
 #include <stdio.h>
 #include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main()
 {
     char x[1000000];
 
-    while (cin.getline(x, 1000000))
+    for (; cin.getline(x, 1000000);)
     {
-        sort(x, x + strlen(x));
-
+        int lwr_constant[26] = {0};
         for (long long int i = 0; i < strlen(x); i++)
         {
             if (x[i] != ' ' && x[i] >= 'a' && x[i] <= 'z')
             {
-                cout << x[i];
+                int lwr_n = x[i] - 'a';
+                lwr_constant[lwr_n] = lwr_constant[lwr_n] + 1;
+            }
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            for (int j = 0; j < lwr_constant[i]; j++)
+            {
+                char keep = (char)('a' + i);
+                cout << keep;
             }
         }
 
@@ -61,4 +34,3 @@ int main()
 
     return 0;
 }
-
